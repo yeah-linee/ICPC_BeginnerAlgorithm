8217c23957742f21e992320ffc56d7f498c2ab9e
yelinjo18 <yelinjo17@gmail.com>
2021-01-08 08:01:55 +0900

    input meetings.

--- Greedy_1931/Greedy_1931/1931.cpp
+++ Greedy_1931/Greedy_1931/1931.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
 int main()
@@ -7,5 +8,15 @@ int main()
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);	cout.tie(0);
 
+	int n;
+	cin >> n;
+
+	vector<pair<int, int>> meeting;
+	while (n--)
+	{
+		int start, end;
+		cin >> start >> end;
+		meeting.push_back({ start, end });
+	}
 
 }
