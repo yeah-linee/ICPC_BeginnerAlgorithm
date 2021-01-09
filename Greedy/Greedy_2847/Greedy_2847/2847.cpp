9eca9b24f1a50c74959bfbfd7d2fab869dbaadcb
yelinjo18 <yelinjo17@gmail.com>
2021-01-09 16:14:35 +0900

    Finish 2847!

--- Greedy/Greedy_2847/Greedy_2847/2847.cpp
+++ Greedy/Greedy_2847/Greedy_2847/2847.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 using namespace std;
+int score[100 + 1];
 
 int main()
 {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);	cout.tie(0);
+	int n_level;	cin >> n_level;
 
+	for (int i = 1; i <= n_level; i++)
+		cin >> score[i];
+
+	int n_dec = 0;
+	for (int i = n_level; i > 1; i--)
+	{
+		while (score[i] <= score[i - 1])
+		{
+			score[i - 1]--;
+			n_dec++;
+		}
+	}
+	cout << n_dec;
 }
