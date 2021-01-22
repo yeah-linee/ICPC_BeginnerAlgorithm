61f7f11bb92ce3f632a71f35746570e8e407fff5
yelinjo18 <yelinjo17@gmail.com>
2021-01-22 15:50:48 +0900

    Make 1757.

---
+++ DynamicProgramming/DP_1757/1757.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int dp[10005][505][2];	// dp[x][y][z]: x초에 y의 지침지수를 가지고 z는 뛰고 있는지를 나타낼 때, 달린 거리의 최댓값
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);	cout.tie(0);
+}
