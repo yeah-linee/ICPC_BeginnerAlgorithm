7e31ce4a2d3039116723d94b68d293be23ccf4e0
yelinjo18 <yelinjo17@gmail.com>
2021-02-03 18:36:07 +0900

    Finish 4949!

--- Stack_Queue_Deque/Stack_4949/4949.cpp
+++ Stack_Queue_Deque/Stack_4949/4949.cpp
@@ -3,12 +3,50 @@
 #include <string>
 using namespace std;
 
-void isBalanced()
+void isBalanced(string input)
 {
-
+	stack<char> stack;
+	for (auto &e : input)
+	{
+		if (e == '[' || e == '(')
+			stack.push(e);
+		else if (e == ']')
+		{
+			if (!stack.empty() && stack.top() == '[')
+				stack.pop();
+			else
+			{
+				cout << "no\n";
+				return;
+			}
+		}
+		else if (e == ')')
+		{
+			if (!stack.empty() && stack.top() == '(')
+				stack.pop();
+			else
+			{
+				cout << "no\n";
+				return;
+			}
+		}
+	}
+	if (stack.empty())		/********Don't forget check it!********/
+		cout << "yes\n";
+	else
+		cout << "no\n";
 }
 
 int main()
 {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);	cout.tie(0);
 
+	while (1)
+	{
+		string input;
+		getline(cin, input);
+		if (input == ".")	break;
+		isBalanced(input);
+	}
 }
