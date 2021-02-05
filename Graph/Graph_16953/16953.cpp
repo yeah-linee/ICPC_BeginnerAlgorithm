39637ca9c84876fccc34114332af0a16b5951792
yelinjo18 <yelinjo17@gmail.com>
2021-02-05 18:31:41 +0900

    Finish 16953!

--- Graph/Graph_16953/16953.cpp
+++ Graph/Graph_16953/16953.cpp
@@ -2,8 +2,37 @@
 #include <queue>
 using namespace std;
 
+long long A, B;
+
+void BFS(int start)
+{
+	queue<pair<long long, long long>> my_queue;
+	my_queue.push({start, 1});
+	while (!my_queue.empty())
+	{
+		pair<long long, long long> tmp = my_queue.front();
+		my_queue.pop();
+		if (tmp.first > B)	continue;
+		else if (tmp.first == B)
+		{
+			cout << tmp.second;
+			return;
+		}
+		else
+		{
+			my_queue.push({ tmp.first * 2, tmp.second + 1 });
+			my_queue.push({ tmp.first * 10 + 1, tmp.second + 1 });
+		}
+	}
+	cout << -1;
+}
+
 int main()
 {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);	cout.tie(0);
+	
+	cin >> A >> B;
+
+	BFS(A);
 }
